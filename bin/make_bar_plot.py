eb4b52686a8fcb39d1e95da89f5749ebb32bd318
nadjano <nnolte@ebi.ac.uk>
2022-05-16 16:30:53 +0100

    added plot

--- bin/make_bar_plot.py
+++ bin/make_bar_plot.py
@@ -3,32 +3,45 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import glob
+import seaborn as sns
 
 def parse_table(results_folder):
   
 
    txtfiles = []
-   for file in glob.glob(str(results_folder)+"/*.txt"):
+   for file in glob.glob(str(results_folder)+"/test*.txt"):
       txtfiles.append(file)
 
-
+   print(txtfiles)
+   list_melt = []
    for file in txtfiles: 
       data=pd.read_csv(file,sep='\t')
+      list_melt.append(data.melt( id_vars = ['tool'],var_name="type", 
+       value_name="percentage"))
    
-   print(data)
 
+   data_concat = pd.concat(list_melt)
+   print(data_concat)
+   return data_concat
 
-def bar_plot():
-   fig = plt.figure()
-   ax = fig.add_axes([0,0,1,1])
-   langs = ['C', 'C++', 'Java', 'Python', 'PHP']
-   students = [23,17,35,29,12]
-   ax.bar(langs,students)
-   plt.show()
-   plt.savefig('snRNA-mapping-rate/bar_plot.png')
+def bar_plot(data_list):
+   # fig = plt.figure()
+   # ax = fig.add_axes([0,0,1,1])
+   # langs = list(data['tool'])
+   # students = list(data['MPR1'])
+   # ax.bar(langs,students)
+   # plt.show()
+   # plt.savefig('bar_plot.png')
+   
+     
+   plot = sns.barplot(x = 'tool', y = 'percentage', hue = 'type', data=data_list, ci ="sd")
+   fig = plot.figure 
+   fig.savefig('bar_plot.png')
 
 
 #main 
 if __name__ == "__main__":
-   parse_table("results")
+   data=parse_table("results")
+
+   bar_plot(data)
     
