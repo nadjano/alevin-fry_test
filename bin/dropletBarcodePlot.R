69719d1ab4abc845783c29e2981a673fdafa5f98
nadjano <nnolte@ebi.ac.uk>
2022-05-31 11:20:03 +0100

    print barcodes to see problem

--- bin/dropletBarcodePlot.R
+++ bin/dropletBarcodePlot.R
@@ -82,7 +82,7 @@ barcode_results <- list(
 plots <- lapply(names(barcode_results), function(name){
   
   barcodes <- barcode_results[[name]]  
-  print(barcodes)
+  print(barcodes['MappedReads'])
   # Get the roryk cutoff
   roryk_count_cutoff <- pick_roryk_cutoff(barcodes$V2)
   
